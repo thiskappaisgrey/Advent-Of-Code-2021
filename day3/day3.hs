66cc4e55e5da6b09eac075660ab8a876218a93d7
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2021-12-20 12:16:04 -0800

    Finish day3 part 2

--- day3/day3.hs
+++ day3/day3.hs
@@ -2,6 +2,7 @@ module Main where
 import qualified Data.Text as T
 import qualified Data.Text.Read as TR
 import qualified Data.Text.IO as TO
+import System.Directory.Internal.Prelude (getArgs)
 -- The idea is to take a list of lists like: [[1, 0, 1], [0, 1, 0] ] and transpose it ito a list like: [[1,0], [0,1], [1, 0]]
 -- Then, I can write a function to count the most common and least common in a list.
 -- Finally, I can convert the binary number into decimal(I need to first reverse the digits and multiply by 2^x or get the lenght and multiply by reverse order?).
@@ -36,9 +37,46 @@ calcGandE list = calcDec rl 0
     pairAdd (a,b) (c,d) = (a + c, b + d)
     calcDec [] acc = (0,0)
     calcDec (a:l) acc = pairAdd (2^acc * (fst a), 2^acc * (snd a)) (calcDec l (acc + 1))
-main = do
+part1Monad = do
   input <- TO.readFile "./input1"
   let a = map (T.unpack) $ T.lines input
   let b = map (map (read . pure :: Char -> Int)) a
   let c =   calcGandE $ map (mostLeastCommon 0 0) (matrixTranspose b)
   print (fst c * snd c)
+
+
+-- Part 2
+-- Part 2 is going to be different than how I did part one. The idea is to put the list of lists into a matrix structure for fast access.
+-- Then, filter out the list of row numbers of the "correct" numbers for the matrix
+
+-- The idea for this one is, you start with a matrix like this(say the index is 0): [[1,0,1,1], [0,0,1,1], [1,1,1,0]] , get the 0th element by transforming it into: [1,0,1]
+-- Then from that list, get the most common element: 1. Then, filter out the stuff with the least comon bit to end up with [[1,0,1,1], [1,1,1,0]]
+filterByBit :: [[Int]] -> Int -> [Int]
+filterByBit [a] _ = a
+filterByBit matrix i = let
+  lWithIthBit = map (!!i) matrix
+  mostCommonBit = fst $ mostLeastCommon 0 0 lWithIthBit
+  in
+  filterByBit  (filter ((== mostCommonBit) . (!! i)) matrix) (i + 1)
+-- for min
+filterByBit2 :: [[Int]] -> Int -> [Int]
+filterByBit2 [a] _ = a
+filterByBit2 matrix i = let
+  lWithIthBit = map (!!i) matrix
+  leastCommonBit = snd $ mostLeastCommon 0 0 lWithIthBit
+  in
+  filterByBit2  (filter ((== leastCommonBit) . (!! i)) matrix) (i + 1)
+binToDec ::   [Int]  -> Int
+binToDec list = calcBin 0 $ reverse list
+  where
+  calcBin _ [] = 0
+  calcBin  acc (n:bin) = (2^acc * n) + calcBin (acc +1) bin
+part2Monad = do
+  name <- getArgs
+  input <- TO.readFile $ head name
+  let a = map T.unpack $ T.lines input
+  let b = map (map (read . pure :: Char -> Int)) a
+  -- print b
+  print $ binToDec $   filterByBit b 0
+  print $ binToDec $   filterByBit2 b 0
+main = part2Monad
